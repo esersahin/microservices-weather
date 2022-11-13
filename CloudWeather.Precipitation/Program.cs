922aa74f36f230fbffae5d68dcc06fe77ebf86f3
Eser SAHIN <eser.sahin@gmail.com>
2022-11-13 18:48:43 +0300

    Add initial dependencies

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip,[FromQuery] int? days) =>
+{
+    return Results.Ok(zip);
+    // var observation = new Observation();
+    // observation.Zip = zip;
+    // observation.Temperature = 70;
+    // observation.Precipitation = 0.0;
+    // return observation;
+});
 
 app.Run();
